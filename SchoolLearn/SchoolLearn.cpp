342a08b50b03c5794435edc4da617d9df68f99af
jestsi <gestse11@gmail.com>
2022-04-04 19:55:42 +0300

    Добавьте файлы проекта.

---
+++ SchoolLearn/SchoolLearn.cpp
@@ -0,0 +1,41 @@
+﻿#include "SchoolLearn.h"
+
+int main()
+{
+	std::string inputValue;
+	std::cout << "Напишите номер автомобиля: ";
+	std::cin >> inputValue;
+
+	setlocale(LC_ALL, "ru");
+
+	if (inputValue.length() != 8 && inputValue.length() != 9) {
+		std::cerr << "Кол-во символов не подходит под кол-во номерного знака" << std::endl;
+		return 1;
+	}
+
+	std::string alhpaChars, digitChars;
+
+	for (size_t i = 0; i < inputValue.length(); i++)
+	{
+		if (isalpha(inputValue[i])) {
+			alhpaChars += inputValue[i];
+			continue;
+		}
+		if (isdigit(inputValue[i])) {
+			alhpaChars += inputValue[i];
+			continue;
+		}
+	}
+
+	if (alhpaChars.length() + digitChars.length() == inputValue.length()) {
+		std::cout << "Да это автомобильный номер!" << std::endl;
+		return 0;
+	}
+	else {
+		std::cout << "Нет это не автомобильный номер!" << std::endl;
+		return 0;
+	}
+
+
+	return 0;
+}
